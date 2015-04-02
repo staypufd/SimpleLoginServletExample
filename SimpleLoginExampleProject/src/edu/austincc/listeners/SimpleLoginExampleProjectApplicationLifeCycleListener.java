d5b9eab1cba9158ba421ed563ca5bcb2746bd7e3
staypufd <harvie.griffith@austincc.edu>
2015-04-02 13:05:09 -0500

    Added Lister for Lifecycle events

---
+++ SimpleLoginExampleProject/src/edu/austincc/listeners/SimpleLoginExampleProjectApplicationLifeCycleListener.java
@@ -0,0 +1,58 @@
+package edu.austincc.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.derby.tools.sysinfo;
+
+/**
+ * Application Lifecycle Listener implementation class SimpleLoginExampleProjectApplicationLifeCycleListener
+ *
+ */
+@WebListener
+public class SimpleLoginExampleProjectApplicationLifeCycleListener implements ServletContextListener, ServletRequestListener {
+
+    /**
+     * Default constructor. 
+     */
+    public SimpleLoginExampleProjectApplicationLifeCycleListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletRequestListener#requestDestroyed(ServletRequestEvent)
+     */
+    public void requestDestroyed(ServletRequestEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("request destroyed");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("context inited");
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("context destroyed");
+    }
+
+	/**
+     * @see ServletRequestListener#requestInitialized(ServletRequestEvent)
+     */
+    public void requestInitialized(ServletRequestEvent arg0)  { 
+         // TODO Auto-generated method stub
+    	
+    	System.out.println("request inited");
+    }
+	
+}
